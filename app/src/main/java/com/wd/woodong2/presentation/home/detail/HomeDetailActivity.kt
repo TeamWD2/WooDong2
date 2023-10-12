98ca0d55159a98c68d38242bbf749bcbeabdf86b
AppCode <lus322@yonsei.ac.kr>
2023-10-12 21:46:14 +0900

    [#12] Ui home_list_item_layout feature HomeFragment HomeItem HomeListAdapter HomeViewModel

--- app/src/main/java/com/wd/woodong2/presentation/home/detail/HomeDetailActivity.kt
+++ app/src/main/java/com/wd/woodong2/presentation/home/detail/HomeDetailActivity.kt
@@ -1,4 +1,17 @@
 package com.wd.woodong2.presentation.home.detail
 
+import android.content.Context
+import android.content.Intent
+import com.wd.woodong2.presentation.home.content.HomeItem
+
 class HomeDetailActivity {
+
+    companion object {
+        private lateinit var HomeDetailItem: HomeItem
+        fun homeDetailActivityNewIntent(context: Context?, homeItem: HomeItem) =
+            Intent(context, HomeDetailActivity::class.java).apply {
+                HomeDetailItem = homeItem
+            }
+    }
+
 }
