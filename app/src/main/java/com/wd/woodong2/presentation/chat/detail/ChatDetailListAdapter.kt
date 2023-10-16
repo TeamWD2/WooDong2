2a45e7767fb945c4818b3a951b688c95a59a7ff8
dannyjoo7 <danny_joo@naver.com>
2023-10-16 16:48:24 +0900

    [#23] 채팅방 메시지 목록 기능 추가

--- app/src/main/java/com/wd/woodong2/presentation/chat/detail/ChatDetailListAdapter.kt
+++ app/src/main/java/com/wd/woodong2/presentation/chat/detail/ChatDetailListAdapter.kt
@@ -1,4 +1,53 @@
 package com.wd.woodong2.presentation.chat.detail
 
-class ChatDetailListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.wd.woodong2.databinding.ChatDetailItemBinding
+
+class ChatDetailListAdapter : ListAdapter<MessageItem, ChatDetailListAdapter.ViewHolder>(
+    object : DiffUtil.ItemCallback<MessageItem>() {
+        override fun areItemsTheSame(
+            oldItem: MessageItem,
+            newItem: MessageItem
+        ): Boolean = oldItem.id == newItem.id
+
+        override fun areContentsTheSame(
+            oldItem: MessageItem,
+            newItem: MessageItem
+        ): Boolean = oldItem == newItem
+    }) {
+
+    abstract class ViewHolder(
+        root: View
+    ) : RecyclerView.ViewHolder(root) {
+        abstract fun onBind(item: MessageItem)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder = OpponentMessageViewHolder(
+        ChatDetailItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ),
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    class OpponentMessageViewHolder(
+        private val binding: ChatDetailItemBinding,
+    ) : ViewHolder(binding.root) {
+        override fun onBind(item: MessageItem) = with(binding) {
+            txtName.text = item.senderId
+            txtChat.text = item.message
+        }
+    }
 }
--- app/src/main/java/com/wd/woodong2/presentation/chat/detail/ChatDetailViewModel.kt
+++ app/src/main/java/com/wd/woodong2/presentation/chat/detail/ChatDetailViewModel.kt
@@ -1,4 +1,65 @@
 package com.wd.woodong2.presentation.chat.detail
 
-class ChatDetailViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.database.FirebaseDatabase
+import com.wd.woodong2.data.repository.MessageRepositoryImpl
+import com.wd.woodong2.domain.usecase.MessageGetItemsUseCase
+import kotlinx.coroutines.launch
+
+class ChatDetailViewModel(
+    private val messageItem: MessageGetItemsUseCase,
+    private val chatKey: String,
+) : ViewModel(
+) {
+    private val _massageList = MutableLiveData<MutableList<MessageItem>>()
+    val messageList: LiveData<MutableList<MessageItem>>
+        get() = _massageList
+
+    init {
+        getMessageItem()
+    }
+
+    private fun getMessageItem() = viewModelScope.launch {
+        runCatching {
+            messageItem(chatKey) { items ->
+                val messageItemList = items?.messageItems?.map {
+                    MessageItem(
+                        id = it.id,
+                        message = it.message,
+                        senderId = it.senderId,
+                        timestamp = it.timestamp,
+                    )
+                }.orEmpty()
+                _massageList.postValue(messageItemList.toMutableList())
+            }
+        }.onFailure {
+            Log.e("danny", it.message.toString())
+        }
+    }
+}
+
+
+class ChatDetailViewModelFactory(
+    private val chatKey: String
+) : ViewModelProvider.Factory {
+
+    private val chatDatabaseReference by lazy {
+        FirebaseDatabase.getInstance().getReference("chats")
+    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ChatDetailViewModel::class.java)) {
+            return ChatDetailViewModel(
+                MessageGetItemsUseCase(MessageRepositoryImpl(chatDatabaseReference)),
+                chatKey
+            ) as T
+        } else {
+            throw IllegalArgumentException("Not found ViewModel class.")
+        }
+    }
 }
