a25b150fc303057447151a0f95b23b4765a1d9e0
dannyjoo7 <danny_joo@naver.com>
2023-10-17 20:27:35 +0900

    [#30] List -> Map으로 데이터 구조 변경

--- app/src/main/java/com/wd/woodong2/data/model/ChatItemResponse.kt
+++ app/src/main/java/com/wd/woodong2/data/model/ChatItemResponse.kt
@@ -11,10 +11,9 @@ data class ChatItemsResponse(
 data class ChatResponse(
     val id: String?,
     @SerializedName("imgProfile") val imgProfile: String?,
-    // 그룹이거나 사람
     @SerializedName("senderId") val senderId: String?,
     @SerializedName("location") val location: String?,
     @SerializedName("timestamp") val timestamp: String?,
     @SerializedName("lastMassage") val lastMassage: String?,
-    @SerializedName("message") val message: List<MessageResponse>?,
+    @SerializedName("message") val message: Map<String, MessageResponse>
 )
