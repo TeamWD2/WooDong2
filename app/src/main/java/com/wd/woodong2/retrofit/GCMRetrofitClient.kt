416b21bdd52ff759562c5c00862365952053ea52
dannyjoo7 <danny_joo@naver.com>
2023-10-25 15:53:00 +0900

    [#43] 피드백 반영 : 파일 분할 및 이름 변경  - RetrofitClient -> KAKAORetrofitClient, GCMRetrofitClient로 분할  - RequestInterceptor -> KAKAORequestInterceptor 이름 변경

--- app/src/main/java/com/wd/woodong2/retrofit/GCMRetrofitClient.kt
+++ app/src/main/java/com/wd/woodong2/retrofit/GCMRetrofitClient.kt
@@ -1,33 +1,9 @@
 package com.wd.woodong2.retrofit
 
-import com.wd.woodong2.presentation.home.map.HomeMapSearchRemoteDatasource
 import com.wd.woodong2.services.fcm.GCMService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
-object KAKAORetrofitClient {
-
-    private const val BASE_URL = "https://dapi.kakao.com"
-
-    private val okHttpClient by lazy {
-        OkHttpClient.Builder()
-            .addInterceptor(RequestInterceptor())
-            .build()
-    }
-
-    private val retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
-    val search: HomeMapSearchRemoteDatasource by lazy {
-        retrofit.create(HomeMapSearchRemoteDatasource::class.java)
-    }
-}
 
 object GCMRetrofitClient {
 
--- app/src/main/java/com/wd/woodong2/retrofit/KAKAORequestInterceptor.kt
+++ app/src/main/java/com/wd/woodong2/retrofit/KAKAORequestInterceptor.kt
@@ -5,7 +5,7 @@ import com.wd.woodong2.WooDongApp
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class RequestInterceptor : Interceptor {
+class KAKAORequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val newRequest = chain.request().newBuilder()
             .addHeader(
