51107d80945b4a47a66ebdc3bbee5f28812fc38a
dannyjoo7 <danny_joo@naver.com>
2023-10-27 10:54:31 +0900

    [#43] SignInActivity  - 로그인 기능 추가

--- app/src/main/java/com/wd/woodong2/presentation/signin/SignInActivity.kt
+++ app/src/main/java/com/wd/woodong2/presentation/signin/SignInActivity.kt
@@ -5,14 +5,21 @@ import android.os.Build
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.wd.woodong2.databinding.SigninActivityBinding
+import com.wd.woodong2.presentation.main.MainActivity
 import com.wd.woodong2.presentation.signup.SignUpActivity
 
 class SignInActivity : AppCompatActivity() {
 
     private var _binding: SigninActivityBinding? = null
     private val binding get() = _binding!!
+
+    private val signInViewModel: SignInViewModel by viewModels {
+        SignInViewModelFactory()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,11 +56,18 @@ class SignInActivity : AppCompatActivity() {
 
         // 로그인 버튼 클릭 시
         btnLogin.setOnClickListener {
-
+            signInViewModel.signIn(editId.text.toString(), editPassword.text.toString())
         }
     }
 
-    private fun initModel() {
-
+    private fun initModel() = with(signInViewModel) {
+        loginResult.observe(this@SignInActivity) { result ->
+            if (result) {
+                Toast.makeText(this@SignInActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                startActivity(MainActivity.newIntentForMain(this@SignInActivity))
+            } else {
+                Toast.makeText(this@SignInActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
