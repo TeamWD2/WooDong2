283a5ada2316606bea6b14e9b3da4c08ce7f038d
dannyjoo7 <danny_joo@naver.com>
2023-10-31 16:06:50 +0900

    [#57] UserPreferencesRepository  - deleteUser 추가

--- app/src/main/java/com/wd/woodong2/domain/repository/UserPreferencesRepository.kt
+++ app/src/main/java/com/wd/woodong2/domain/repository/UserPreferencesRepository.kt
@@ -3,4 +3,5 @@ package com.wd.woodong2.domain.repository
 interface UserPreferencesRepository {
     fun saveUser(userId: String, isLoggedIn: Boolean)
     fun getUser(): String?
+    fun deleteUser()
 }
