23189b387a5909198afc745c30bb60cdc645975b
dannyjoo7 <danny_joo@naver.com>
2023-11-02 12:00:10 +0900

    [#63] token 추가  - saveUser : token 추가  - UserPreferencesRepositoryImpl : const val로 분리

--- app/src/main/java/com/wd/woodong2/data/repository/UserPreferencesRepositoryImpl.kt
+++ app/src/main/java/com/wd/woodong2/data/repository/UserPreferencesRepositoryImpl.kt
@@ -9,25 +9,30 @@ class UserPreferencesRepositoryImpl(
 
     companion object {
         const val TAG = "UserPreferencesRepository"
+
+        const val USER_ID = "userId"
+        const val IS_LOGGED_IN = "isLoggedIn"
+        const val TOKEN = "token"
     }
 
-    override fun saveUser(userId: String, isLoggedIn: Boolean) {
+    override fun saveUser(userId: String, isLoggedIn: Boolean, token: String) {
         sharedPreferences.edit().apply {
-            putString("userId", userId)
-            putBoolean("isLoggedIn", isLoggedIn)
+            putString(USER_ID, userId)
+            putBoolean(IS_LOGGED_IN, isLoggedIn)
+            putString(TOKEN, token)
         }.apply()
     }
 
     override fun getUser(): String? {
-        return if (sharedPreferences.getBoolean("isLoggedIn", false)) {
-            sharedPreferences.getString("userId", null)
+        return if (sharedPreferences.getBoolean(IS_LOGGED_IN, false)) {
+            sharedPreferences.getString(USER_ID, null)
         } else null
     }
 
     override fun deleteUser() {
         sharedPreferences.edit().apply {
-            remove("userId")
-            remove("isLoggedIn")
+            remove(USER_ID)
+            remove(IS_LOGGED_IN)
             apply()
         }
     }
