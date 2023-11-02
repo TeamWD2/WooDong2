e6d6352f86ffac9e6a83d095ff25b79de54155c2
dannyjoo7 <danny_joo@naver.com>
2023-11-02 13:59:28 +0900

    [#63] 토큰 받아오는 기능 추가  - GetFirebaseTokenUseCase : 클라이언트 토큰 불러옴  - SignInViewModel : 로그인 시 토큰 발급받는 부분 추가

--- app/src/main/java/com/wd/woodong2/presentation/signin/SignInViewModel.kt
+++ app/src/main/java/com/wd/woodong2/presentation/signin/SignInViewModel.kt
@@ -9,19 +9,26 @@ import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import com.wd.woodong2.R
 import com.wd.woodong2.data.repository.UserPreferencesRepositoryImpl
 import com.wd.woodong2.data.repository.UserRepositoryImpl
+import com.wd.woodong2.domain.provider.FirebaseTokenProvider
+import com.wd.woodong2.domain.usecase.GetFirebaseTokenUseCase
 import com.wd.woodong2.domain.usecase.SignInGetUserUseCase
 import com.wd.woodong2.domain.usecase.SignInSaveUserUseCase
 import com.wd.woodong2.domain.usecase.UserSignInUseCase
 import com.wd.woodong2.presentation.provider.ContextProvider
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class SignInViewModel(
     private val signInUser: UserSignInUseCase,
     private val saveUser: SignInSaveUserUseCase,
     private val getUser: SignInGetUserUseCase,
+    private val getFirebaseToken: GetFirebaseTokenUseCase,
 ) : ViewModel(
 ) {
     companion object {
@@ -35,14 +42,22 @@ class SignInViewModel(
     fun signIn(id: String, pw: String, isAutoLogIn: Boolean) {
         viewModelScope.launch {
             runCatching {
-                signInUser(id, pw).collect { isSuccess ->
-                    _loginResult.value = isSuccess
-
-                    // 기기에 정보 저장
-                    if (_loginResult.value == true) {
-                        saveUser(id, isAutoLogIn)
+                signInUser(id, pw)
+                    .flatMapConcat { isSuccess ->
+                        if (isSuccess) {
+                            getFirebaseToken().map { token -> Pair(isSuccess, token) }
+                        } else {
+                            flowOf(Pair(false, ""))
+                        }
                     }
-                }
+                    .collect { (isSuccess, token) ->
+                        _loginResult.value = isSuccess
+
+                        if (isSuccess && token != "") {
+                            saveUser(id, isAutoLogIn)
+//                            updateUserToken(id)
+                        }
+                    }
             }.onFailure {
                 Log.e(TAG, it.message.toString())
                 _loginResult.value = false
@@ -67,7 +82,8 @@ class SignInViewModelFactory(
     private val userRepositoryImpl by lazy {
         UserRepositoryImpl(
             FirebaseDatabase.getInstance().getReference("users"),
-            Firebase.auth
+            Firebase.auth,
+            FirebaseTokenProvider(FirebaseMessaging.getInstance())
         )
     }
 
@@ -81,6 +97,7 @@ class SignInViewModelFactory(
                 UserSignInUseCase(userRepositoryImpl),
                 SignInSaveUserUseCase(userPreferencesRepository),
                 SignInGetUserUseCase(userPreferencesRepository),
+                GetFirebaseTokenUseCase(),
             ) as T
         } else {
             throw IllegalArgumentException("Not found ViewModel class.")
