37308e16c81aae1fb45dc1d334172c6ed457334d
dannyjoo7 <danny_joo@naver.com>
2023-11-03 14:12:51 +0900

    [#61] 디자인 변경  - 스플래시 이미지 상태바 삭제  - 자동 로그인 체크박스 색 변경

--- app/src/main/java/com/wd/woodong2/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/wd/woodong2/presentation/splash/SplashActivity.kt
@@ -1,9 +1,12 @@
 package com.wd.woodong2.presentation.splash
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.wd.woodong2.databinding.SplashActivityBinding
 import com.wd.woodong2.presentation.signin.SignInActivity
@@ -17,6 +20,16 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = SplashActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, SignInActivity::class.java))
