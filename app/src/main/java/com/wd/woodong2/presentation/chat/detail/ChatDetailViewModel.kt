683b99d3bc6b55819298279dc7bcf5d758832e56
dannyjoo7 <danny_joo@naver.com>
2023-11-04 13:42:53 +0900

    [#62] UID 받아오는 메소드 추가 및 닉네임 설정 코드 추가

--- app/src/main/java/com/wd/woodong2/presentation/chat/detail/ChatDetailViewModel.kt
+++ app/src/main/java/com/wd/woodong2/presentation/chat/detail/ChatDetailViewModel.kt
@@ -6,19 +6,31 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import com.wd.woodong2.data.repository.ChatRepositoryImpl
+import com.wd.woodong2.data.repository.UserRepositoryImpl
+import com.wd.woodong2.domain.provider.FirebaseTokenProvider
 import com.wd.woodong2.domain.usecase.ChatGetMessageItemsUseCase
 import com.wd.woodong2.domain.usecase.ChatSendMessageUseCase
+import com.wd.woodong2.domain.usecase.SignInGetUserUIDUseCase
+import com.wd.woodong2.domain.usecase.UserGetItemUseCase
 import com.wd.woodong2.presentation.chat.content.ChatItem
 import kotlinx.coroutines.launch
 
 class ChatDetailViewModel(
     private val getMessageItem: ChatGetMessageItemsUseCase,
     private val sendMessageItem: ChatSendMessageUseCase,
-    private val userId: String
+    private val getUserUID: SignInGetUserUIDUseCase,
+    private val getUser: UserGetItemUseCase,
 ) : ViewModel(
 ) {
+    companion object {
+        const val TAG = "ChatDetailViewModel"
+    }
+
     private val _massageList = MutableLiveData<MutableList<MessageItem>>()
     val messageList: LiveData<MutableList<MessageItem>>
         get() = _massageList
@@ -26,8 +38,26 @@ class ChatDetailViewModel(
     private val _isLoading: MutableLiveData<Boolean> = MutableLiveData()
     val isLoading: LiveData<Boolean> get() = _isLoading
 
+    private lateinit var uid: String
+    private lateinit var name: String
+
     init {
         getMessageItem()
+        uid = getUserUID() ?: ""
+        getUserEntity()
+    }
+
+    private fun getUserEntity() = viewModelScope.launch {
+        runCatching {
+            _isLoading.value = true
+            getUser(uid).collect { user ->
+                name = user?.name ?: "(알 수 없음)"
+                _isLoading.value = false
+            }
+        }.onFailure {
+            Log.e(TAG, it.message.toString())
+            _isLoading.value = false
+        }
     }
 
     private fun getMessageItem() = viewModelScope.launch {
@@ -40,7 +70,8 @@ class ChatDetailViewModel(
                         content = messageResponse.content,
                         senderId = messageResponse.senderId,
                         timestamp = messageResponse.timestamp,
-                        isMyMessage = messageResponse.senderId == userId
+                        isMyMessage = messageResponse.senderId == uid,
+                        nickname = messageResponse.nickname
                     )
                 }?.sortedBy { it.timestamp } ?: emptyList()
                 _massageList.postValue(messageItemList.toMutableList())
@@ -55,7 +86,7 @@ class ChatDetailViewModel(
     fun sendMessage(message: String) = viewModelScope.launch {
         runCatching {
             // Test
-            sendMessageItem(userId, message)
+            sendMessageItem(uid, message, name)
         }.onFailure {
             Log.e("danny", it.message.toString())
         }
@@ -64,9 +95,7 @@ class ChatDetailViewModel(
 
 class ChatDetailViewModelFactory(
     private val chatItem: ChatItem,
-    private val userId: String,
 ) : ViewModelProvider.Factory {
-
     private val chatRepository by lazy {
         ChatRepositoryImpl(
             when (chatItem) {
@@ -83,12 +112,21 @@ class ChatDetailViewModelFactory(
         )
     }
 
+    private val userRepositoryImpl by lazy {
+        UserRepositoryImpl(
+            FirebaseDatabase.getInstance().getReference("users"),
+            Firebase.auth,
+            FirebaseTokenProvider(FirebaseMessaging.getInstance())
+        )
+    }
+
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ChatDetailViewModel::class.java)) {
             return ChatDetailViewModel(
                 ChatGetMessageItemsUseCase(chatRepository),
                 ChatSendMessageUseCase(chatRepository),
-                userId
+                SignInGetUserUIDUseCase(userRepositoryImpl),
+                UserGetItemUseCase(userRepositoryImpl),
             ) as T
         } else {
             throw IllegalArgumentException("Not found ViewModel class.")
